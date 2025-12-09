0b2f8fc89e6d2d39639dc87e91bc878a146f0a8c
Matt Kramer <mjkramer@users.noreply.github.com>
2025-12-09 15:28:28 -0800

    fixup

--- src/flow2supera/driver.py
+++ src/flow2supera/driver.py
@@ -269,7 +269,7 @@ class SuperaDriver:
             while not ancestor.id == ancestor.parent_id:
                 ancestor = supera_event[ancestor.parent_id]
 
-            supera_event[index].ancestor_id = ancestor.track_id
+            supera_event[index].ancestor_id = ancestor.part.trackid
         # A-4 finished
 
 
